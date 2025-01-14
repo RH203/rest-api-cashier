24f3f5f97b659ca2df008e321688e685c9c03e8a
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2025-01-14 16:32:12 +0700

    ✨ feat(app/Models/Payments.php): add fillable properties for payment details in Payments model

--- app/Models/Payments.php
+++ app/Models/Payments.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payments extends Model
 {
+
+  protected $fillable = ['amount_paid', 'change_due', 'payment_method', 'order_id'];
     public function orders()
     {
       return $this->belongsTo(Orders::class);
